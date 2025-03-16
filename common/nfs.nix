7aa62bd49d2530e8f8d9ede26c6a36102368e987
Chris Dell <cjdell@gmail.com>
2025-03-16 22:57:41 +0000

    Update NFS

--- common/nfs.nix
+++ common/nfs.nix
@@ -3,7 +3,20 @@
 { 
   fileSystems."/ds-public" = 
     { device = "192.168.49.21:/sas-16tb/ds-public";
-      fsType = "nfs";
-      options = [ "rw" "hard" "intr" "rsize=1048576" "wsize=1048576" "acregmin=3" "acregmax=60" "acdirmin=30" "acdirmax=60" "noatime" "nodiratime" ];
+      fsType = "nfs4"; # Use NFSv4 for better performance
+      options = [
+        "rw"
+        "hard"
+        "intr"
+        "rsize=1048576"
+        "wsize=1048576"
+        "proto=tcp" # Ensure TCP is used for NFS
+        "acregmin=3"
+        "acregmax=60"
+        "acdirmin=30"
+        "acdirmax=120" # Increase dir attribute cache time
+        "noatime"
+        "nodiratime"
+      ];
     };
 }
