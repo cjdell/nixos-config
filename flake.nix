1176a3e0b63b0f65df2d7285060ff3cee7577ac7
Chris Dell <cjdell@gmail.com>
2025-03-22 22:33:42 +0000

    Reformat flake

--- flake.nix
+++ flake.nix
@@ -14,148 +14,150 @@
   };
 
   outputs = { self, nixpkgs, nixos-hardware, home-manager }@attrs: {
-    nixosConfigurations.precision-nixos =
-      let
-        system = "x86_64-linux";
-      in
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-        modules = [
-          ./common/desktop.nix
-          ./common/nfs.nix
-          ./common/system.nix
-          ./common/wine.nix
-          ./users/cjdell/permissions.nix
-          ./machines/precision
-          ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
-          nixos-hardware.nixosModules.dell-precision-5520
-        ];
-      };
+    nixosConfigurations = {
+      precision-nixos =
+        let
+          system = "x86_64-linux";
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+          modules = [
+            ./common/desktop.nix
+            ./common/nfs.nix
+            ./common/system.nix
+            ./common/wine.nix
+            ./users/cjdell/permissions.nix
+            ./machines/precision
+            ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
+            nixos-hardware.nixosModules.dell-precision-5520
+          ];
+        };
 
-    nixosConfigurations.haswellatx-nixos =
-      let
-        system = "x86_64-linux";
-      in
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-        modules = [
-          ./common/desktop.nix
-          ./common/folding-at-home.nix
-          ./common/nfs.nix
-          ./common/nosleep.nix
-          ./common/sunshine.nix
-          ./common/system.nix
-          ./common/wine.nix
-          ./machines/haswellatx
-          ./users/cjdell/permissions.nix
-          ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cjdell = import ./users/cjdell/home.nix;
-          }
-        ];
-      };
+      haswellatx-nixos =
+        let
+          system = "x86_64-linux";
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+          modules = [
+            ./common/desktop.nix
+            ./common/folding-at-home.nix
+            ./common/nfs.nix
+            ./common/nosleep.nix
+            ./common/sunshine.nix
+            ./common/system.nix
+            ./common/wine.nix
+            ./machines/haswellatx
+            ./users/cjdell/permissions.nix
+            ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.cjdell = import ./users/cjdell/home.nix;
+            }
+          ];
+        };
 
-    nixosConfigurations.haswellmatx-nixos =
-      let
-        system = "x86_64-linux";
-      in
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-        modules = [
-          ./common/desktop.nix
-          ./common/folding-at-home.nix
-          ./common/nfs.nix
-          ./common/nosleep.nix
-          ./common/sunshine.nix
-          ./common/system.nix
-          ./common/wine.nix
-          ./machines/haswellmatx
-          ./users/cjdell/permissions.nix
-          ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cjdell = import ./users/cjdell/home.nix;
-          }
-        ];
-      };
+      haswellmatx-nixos =
+        let
+          system = "x86_64-linux";
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+          modules = [
+            ./common/desktop.nix
+            ./common/folding-at-home.nix
+            ./common/nfs.nix
+            ./common/nosleep.nix
+            ./common/sunshine.nix
+            ./common/system.nix
+            ./common/wine.nix
+            ./machines/haswellmatx
+            ./users/cjdell/permissions.nix
+            ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.cjdell = import ./users/cjdell/home.nix;
+            }
+          ];
+        };
 
-    nixosConfigurations.kabylakeitx-nixos =
-      let
-        system = "x86_64-linux";
-      in
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-        modules = [
-          ./common/desktop.nix
-          ./common/folding-at-home.nix
-          ./common/nfs.nix
-          ./common/nosleep.nix
-          ./common/sunshine.nix
-          ./common/system.nix
-          ./machines/kabylakeitx
-          ./users/cjdell/permissions.nix
-          ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cjdell = import ./users/cjdell/home.nix;
-          }
-        ];
-      };
+      kabylakeitx-nixos =
+        let
+          system = "x86_64-linux";
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+          modules = [
+            ./common/desktop.nix
+            ./common/folding-at-home.nix
+            ./common/nfs.nix
+            ./common/nosleep.nix
+            ./common/sunshine.nix
+            ./common/system.nix
+            ./machines/kabylakeitx
+            ./users/cjdell/permissions.nix
+            ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.cjdell = import ./users/cjdell/home.nix;
+            }
+          ];
+        };
 
-    nixosConfigurations.coffeelakelenovo-nixos =
-      let
-        system = "x86_64-linux";
-      in
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-        modules = [
-          ./common/desktop.nix
-          ./common/folding-at-home.nix
-          ./common/nfs.nix
-          ./common/nosleep.nix
-          ./common/sunshine.nix
-          ./common/system.nix
-          ./machines/coffeelakelenovo
-          ./users/cjdell/permissions.nix
-          ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cjdell = import ./users/cjdell/home.nix;
-          }
-        ];
-      };
+      coffeelakelenovo-nixos =
+        let
+          system = "x86_64-linux";
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+          modules = [
+            ./common/desktop.nix
+            ./common/folding-at-home.nix
+            ./common/nfs.nix
+            ./common/nosleep.nix
+            ./common/sunshine.nix
+            ./common/system.nix
+            ./machines/coffeelakelenovo
+            ./users/cjdell/permissions.nix
+            ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.cjdell = import ./users/cjdell/home.nix;
+            }
+          ];
+        };
 
-    nixosConfigurations.arcadebox-101 =
-      let
-        system = "x86_64-linux";
-      in
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; permittedInsecurePackages = [ "freeimage-unstable-2021-11-01" ]; }; };
-        modules = [
-          ./common/arcade.nix
-          ./common/desktop.nix
-          ./common/sunshine.nix
-          ./common/nosleep.nix
-          ./common/system.nix
-          ./users/user.nix
-          ./machines/arcadebox-101
-          ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
-        ];
-      };
+      arcadebox-101 =
+        let
+          system = "x86_64-linux";
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; permittedInsecurePackages = [ "freeimage-unstable-2021-11-01" ]; }; };
+          modules = [
+            ./common/arcade.nix
+            ./common/desktop.nix
+            ./common/sunshine.nix
+            ./common/nosleep.nix
+            ./common/system.nix
+            ./users/user.nix
+            ./machines/arcadebox-101
+            ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; }) # For "nix shell"
+          ];
+        };
+    };
   };
 }
