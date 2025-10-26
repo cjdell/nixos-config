a03ad61cbf72091ff4e4de94c6e68accc46779f7
Chris Dell <cjdell@gmail.com>
2025-10-26 19:02:02 +0000

    Update lock file

--- switch-pxe.sh
+++ switch-pxe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-nix flake lock --update-input pxe-server
+nix flake update pxe-server
 # sudo umount -f -l /exports/pxe-server-nix-store
 sudo umount -f -l /exports/pxe-server-squashfs
 sudo nixos-rebuild switch --flake . --impure
