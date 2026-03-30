cf9679196a8fa1506361497e1e6bb29e7b40b084
Chris Dell <cjdell@gmail.com>
2026-03-30 23:28:19 +0100

    Update Tailscale key

--- machines/N100-NAS/hardware-configuration.nix
+++ machines/N100-NAS/hardware-configuration.nix
@@ -113,7 +113,7 @@ in
   };
 
   fileSystems."/electronerds/ds-private" = {
-    device = "//10.47.98.10/Private";
+    device = "//colin.grafton.tailscale/Private";
     fsType = "cifs";
     options = [
       "username=admin"
