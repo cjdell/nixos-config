7d6eeabe278e6fb2b0e37c88c098e29c766eb7e5
Chris Dell <cjdell@gmail.com>
2026-04-02 18:34:19 +0100

    GEN8-NAS add backup jobs

--- machines/GEN8-NAS/backup.nix
+++ machines/GEN8-NAS/backup.nix
@@ -1,11 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  options,
-  ...
-}:
+{ options, ... }:
 
 {
   services.sanoid = {
@@ -27,29 +20,40 @@
   };
 
   system.activationScripts.installBackupKey = ''
-    cat << EOF > /var/lib/syncoid/backup.key
-    ${builtins.readFile "/home/cjdell/nixos-config/secrets/backup.key"}
-    EOF
+    chmod 0400 /var/lib/syncoid/backup.key
   '';
 
-  # services.syncoid = {
-  #   enable = true;
-  #   interval = "*:35"; # run this less often than sanoid (every hour at 35 mins)
-  #   #commonArgs = [ "--debug" ];
-  #   commands = {
-  #     "ds-public" = {
-  #       sshKey = "/var/lib/syncoid/backup.key";
-  #       source = "sas-16tb/ds-public";
-  #       target = "backup@10.47.35.20:dbthr33/ds-external-backups/grafton";
-  #       sendOptions = "w c";
-  #       extraArgs = [ "--sshoption=StrictHostKeyChecking=off" ];
-  #     };
-  #   };
-  #   localSourceAllow = options.services.syncoid.localSourceAllow.default ++ [
-  #     "mount"
-  #   ];
-  #   localTargetAllow = options.services.syncoid.localTargetAllow.default ++ [
-  #     "destroy"
-  #   ];
-  # };
+  # sudo systemctl start syncoid-ds-public
+  # journalctl -u syncoid-ds-public -f
+
+  # sudo systemctl start syncoid-ds-cjdell
+  # journalctl -u syncoid-ds-cjdell -f
+
+  services.syncoid = {
+    enable = true;
+    interval = "*:35"; # run this less often than sanoid (every hour at 35 mins)
+    #commonArgs = [ "--debug" ];
+    commands = {
+      "ds-public" = {
+        sshKey = "/var/lib/syncoid/backup.key";
+        source = "sas-24tb/ds-public";
+        target = "backup@n40l-nas.grafton.tailscale:sas-16tb/ds-external-backups/ds-sas-24tb/ds-public";
+        sendOptions = "w c";
+        extraArgs = [ "--sshoption=StrictHostKeyChecking=off" ];
+      };
+      "ds-cjdell" = {
+        sshKey = "/var/lib/syncoid/backup.key";
+        source = "sas-24tb/ds-cjdell";
+        target = "backup@n40l-nas.grafton.tailscale:sas-16tb/ds-external-backups/ds-sas-24tb/ds-cjdell";
+        sendOptions = "w c";
+        extraArgs = [ "--sshoption=StrictHostKeyChecking=off" ];
+      };
+    };
+    localSourceAllow = options.services.syncoid.localSourceAllow.default ++ [
+      "mount"
+    ];
+    localTargetAllow = options.services.syncoid.localTargetAllow.default ++ [
+      "destroy"
+    ];
+  };
 }
