0451fa776b6ea33484b6e37bc6ba412286323d75
Chris Dell <cjdell@gmail.com>
2026-08-14 10:25:38 +0100

    Add ds-home to NFS

--- machines/GEN8-NAS/nfs.nix
+++ machines/GEN8-NAS/nfs.nix
@@ -3,5 +3,6 @@
   services.nfs.server.exports = ''
     /sas-24tb/ds-downloads  192.168.0.0/16(rw,nohide,insecure,no_subtree_check) 10.47.0.0/16(rw,nohide,insecure,no_subtree_check)
     /sas-24tb/ds-public     192.168.0.0/16(rw,nohide,insecure,no_subtree_check)
+    /sas-24tb/ds-home       192.168.0.0/16(rw,nohide,insecure,no_subtree_check,async,no_auth_nlm,no_root_squash)
   '';
 }
